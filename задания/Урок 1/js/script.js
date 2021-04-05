cc5faa5392d953f9a71c8000d8874e51f448e4b0
Alexey Novoselov <epama96@gmail.com>
2021-04-05 13:19:31 +0400

    урок 1

--- задания/Урок 1/js/script.js
+++ задания/Урок 1/js/script.js
@@ -20,14 +20,3 @@ console.log(addExpenses.split(','));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
-
-
-let nun = 266219;
-// let resultOne = [nun].toString().split('').map(n => n*1); - удалось разделить число на масивы, но перемножить не смог
-//                                                             Воспользовался функцией
-function resultTwo(number) {
-    return [...number.toString()].reduce((p, v) => p * v);
-}
-let mul = (resultTwo(nun))**3 + " ";
-console.log(mul.substring(0, 2));
-
