232253b37ac83272b098e3fab5c0dc2bf85605b2
Alexey Novoselov <epama96@gmail.com>
2021-04-05 13:33:31 +0400

    Урок 2 усложненный

--- задания/Урок 1/js/script.js
+++
@@ -1,9 +0,0 @@
-let nun = 266219;
-// let resultOne = [nun].toString().split('').map(n => n*1); - удалось разделить число на масивы, но перемножить не смог
-//                                                             Воспользовался функцией
-function resultTwo(number) {
-    return [...number.toString()].reduce((p, v) => p * v);
-}
-let mul = (resultTwo(nun))**3 + " ";
-console.log(mul.substring(0, 2));
-
