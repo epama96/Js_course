77bb4b921a45a4053146487c1a851bfc1b4e3a72
Alexey Novoselov <epama96@gmail.com>
2021-04-11 12:36:16 +0400

    урок 5 усложненый

--- js/script.js
+++ js/script.js
@@ -1,82 +1,19 @@
-'use strick'
-let isNumber = function(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-  };
+let arr = ['22522', '950003', '1234423', '45302', '45678', '9876', '2345'];
 
-let money;
-let income = 'work';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Еда, комуналка, такси');
-let deposite = confirm('Есть ли у вас депозит в банке?');
-let mission = 200000;
-let period = 2;
-
-let showTypeOf = function(data) {
-    console.log(data, typeof(data));
-}
-
-
-let start = function () {
-    do {
-      money = prompt('Ваш месячный доход?');
-    }
-    while (!isNumber(money));
-  };
-start();
-
-
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposite);
-
-console.log(addExpenses.split(','));
- 
-
-let expenses = [];
-function getExpensesMonth() { 
-    let sum = 0;
-  
-    for (let i = 0; i < 2; i++) {
-        expenses[i] = prompt('Введите обязательную статью расходов?');
-        sum = +prompt('Во сколько это обойдется?');
-          if (isNumber(sum)) {
-            sum += sum;
-          } else { do {
-            sum = prompt('Во сколько это обойдется?');
-          }
-          while (!isNumber(sum));
-        }
-    }
-    console.log(sum);
-    return sum;
+arr.forEach(em => {
+  if (String(em).search(/(2|4)/) === 0) {
+    console.log(em);
   }
+});
 
-let expensesAmount = getExpensesMonth();
-console.log('Расходы за месяц:', expensesAmount)
-
-function getAccumulatedMonth() { 
-    return (money - expensesAmount);
+let n = 100;
+nextPrime:
+for (let i = 2; i <= n; i++) { 
+  let a = 2;
+  for (; a < i; a++) { 
+    if (i % a === 0) {
+    continue nextPrime; 
+    }
+  }
+  console.log(`${i}: Простое число. Делители числа: 1 и `, a);
 }
-getAccumulatedMonth();
-
-function accumulatedMonth() {
-    console.log (getAccumulatedMonth());
-}
-accumulatedMonth();
-
-function getTargetMonth () {
-    return mission / getAccumulatedMonth();
-}
-function budgetDay() {
-    return getAccumulatedMonth() / 30;
-} 
-if (getTargetMonth> 0) {
-    console.log(`Цель будет достигнута за: ${getTargetMonth} месяцев`);
-  } else {
-    console.log('Цель не будет достигнута');
-}
-
-showTypeOf(budgetDay());
-showTypeOf(getExpensesMonth());
-showTypeOf(addExpenses.split(','));
-showTypeOf(getTargetMonth());
-showTypeOf(budgetDay());
